f6084bb760b29ddaed90e70cc531285846d793ac
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-11-01 16:53:10 +0000

    figuring out array output

--- HW10_P3/main3.cpp
+++ HW10_P3/main3.cpp
@@ -10,13 +10,14 @@ void sortArray(int myArray[], int ARRAY_SIZE);
 void printArray(const int myArray[], int ARRAY_SIZE);
 
 int main() {
-  int newMax, index;
+  int newMax, newArraySize, index;
   int N;
   cout << "Enter size of N: ";
   cin >> N;
 
   const int ARRAY_SIZE = N;
   int myArray[ARRAY_SIZE];
+  int myNewArray[] = {0};
 
   cout << "Enter " << N << " integers: ";
   for(index = 0; index < ARRAY_SIZE; index++){
@@ -24,6 +25,11 @@ int main() {
     cin >> myArray[index];
     }
 
+  for (index = 0; index < myArray[newMax]; index++) {
+    newArraySize = myArray[newMax];
+    
+    cout << newArraySize << endl;
+  }
   for(index = 0; index < N; index++){
     if(index == 0){
       newMax = myArray[0];
@@ -33,6 +39,8 @@ int main() {
     newMax = myArray[index]; 
     cout << newMax << " ";
       } 
+    cin >> myArray[newMax];
+    myArray[newMax] = myNewArray[];
     }
    return 0;
   }
