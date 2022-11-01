e6c1532dd276a9c23c7dda8efbf0ffffd34b88dc
johnthom11370 <112103129+jcthompson11370@users.noreply.github.com>
2022-11-01 04:44:55 +0000

    include sort array function

--- .ccls-cache/@home@runner@HW10Probs/HW10_P3@main3.cpp
+++ .ccls-cache/@home@runner@HW10Probs/HW10_P3@main3.cpp
@@ -6,33 +6,56 @@
 
 using namespace std;
 
-int main() {
-    
-bool sync_with_stdio( bool sync = true );
-  
+void sortArray(int myArray[], int ARRAY_SIZE);
 
-  cin.tie(0);
+int main() {
+  int A;
+  int index = 0;
+  int N;
+  cout << "Enter size of N: ";
+  cin >> N;
+
+  const int ARRAY_SIZE = N;
+  int myArray[ARRAY_SIZE];
+
+  cout << "Enter " << N << " integers: ";
+  for(int index = 0; index < ARRAY_SIZE; index++){
   
-  int n; 
-  int max = -1; 
-  int count = 0;
-  string s = "";
-  
-  cin >> n;
-  
-  for (;n-- > 0;) {
-    int a;
-    
-    cin >> a;
-    
-    if (a > max) {
-      max = a;
-      s += to_string(a) + " ";
-      count++;
-        }
+    cin >> myArray[A];
+    }
+
+  for(int index = 0; index < N; index++){
+    if(index == 0){
+      A = myArray[0];
+      cout << A << endl;
+    }
+    else if(A < myArray[index])
+      sortArray( myArray[], ARRAY_SIZE);
+    }
+   return 0;
   }
-  
-  cout << count << '\n' << s;
-  
-  return 0;
+void sortArray(int myArray[], int ARRAY_SIZE){
+  int index, smallestIndex, location, temp;
+
+  for(int index = 0; index < ARRAY_SIZE -1; index++){
+    
+   smallestIndex = index;
+    
+    for(int location = index +1 ; location < ARRAY_SIZE; location++){
+     
+      if(myArray[location] < myArray[smallestIndex])
+     
+        smallestIndex = location; // location of smallest index
+   } 
+
+    // make swap
+    temp = myArray[smallestIndex];
+    myArray[smallestIndex] = myArray[index];
+    myArray[index] = temp;
+    
+  }
 }
+    
+ 
+  
+    
--- HW10_P3/main3.cpp
+++ HW10_P3/main3.cpp
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-void sortArray(int list[], int length);
+void sortArray(int myArray[], int ARRAY_SIZE);
 
 int main() {
   int A;
+  int index = 0;
   int N;
   cout << "Enter size of N: ";
   cin >> N;
@@ -17,21 +18,44 @@ int main() {
   const int ARRAY_SIZE = N;
   int myArray[ARRAY_SIZE];
 
+  cout << "Enter " << N << " integers: ";
   for(int index = 0; index < ARRAY_SIZE; index++){
-    cin >> myArray[index];
+  
+    cin >> myArray[A];
     }
 
   for(int index = 0; index < N; index++){
-    if(index = 0){
+    if(index == 0){
       A = myArray[0];
       cout << A << endl;
     }
-    else (A < myArray[index]){
-      cout << myArray[index];
-      A = myArray[index];
+    else if(A < myArray[index])
+      sortArray( myArray[], ARRAY_SIZE);
     }
+   return 0;
   }
+void sortArray(int myArray[], int ARRAY_SIZE){
+  int index, smallestIndex, location, temp;
+
+  for(int index = 0; index < ARRAY_SIZE -1; index++){
     
-  return 0;
+   smallestIndex = index;
+    
+    for(int location = index +1 ; location < ARRAY_SIZE; location++){
+     
+      if(myArray[location] < myArray[smallestIndex])
+     
+        smallestIndex = location; // location of smallest index
+   } 
+
+    // make swap
+    temp = myArray[smallestIndex];
+    myArray[smallestIndex] = myArray[index];
+    myArray[index] = temp;
+    
   }
+}
+    
+ 
+  
     
